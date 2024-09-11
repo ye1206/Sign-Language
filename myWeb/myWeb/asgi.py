337e9a9544379a1ec1259ba738868553dac4838e
ALICE\alice <alicecheng.126@gmail.com>
2024-09-12 02:50:53 +0800

    fix text direction

--- myWeb/myWeb/asgi.py
+++ myWeb/myWeb/asgi.py
@@ -1,16 +1,16 @@
-"""
-ASGI config for myWeb project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import webs.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myWeb.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),  # HTTP request
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            webs.routing.websocket_urlpatterns  # WebSocket
+        )
+    ),
+})
--- myWeb/script/new_put_text.py
+++ myWeb/script/new_put_text.py
@@ -14,12 +14,21 @@ def cv2_chinese_text(img, text, left, top, text_color=(0, 255, 0)):
     # 判断是否OpenCV图片类型
     if (isinstance(img, np.ndarray)):
         img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
-    # 创建一个可以在给定图像上绘图的对象
-    draw = ImageDraw.Draw(img)
+
     # 字体的格式
     font_style = ImageFont.truetype(settings.FONT_DIR,
                                     text_size, encoding="utf-8")
+
+    # 创建一个可以在给定图像上绘图的对象
+    draw = ImageDraw.Draw(img)
+    text_img = Image.new('RGBA', img.size, (255, 255, 255, 0))
+    text_draw = ImageDraw.Draw(text_img)
+    text_draw.text((left, top), text, text_color, font=font_style)
+
+    text_img = text_img.transpose(Image.FLIP_LEFT_RIGHT)
+    img.paste(text_img, (0, 0), text_img)
+
     # 绘制文本
-    draw.text((left, top), text, text_color, font=font_style)
+    # draw.text((left, top), text, text_color, font=font_style)
     # 转换回OpenCV格式
     return cv2.cvtColor(np.asarray(img), cv2.COLOR_RGB2BGR)
